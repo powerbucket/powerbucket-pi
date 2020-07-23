1afacc8fe696cd50e0315aadf7db1f4aceaf5f43
Joe Abbate <joseph.abbate7@gmail.com>
2020-07-23 09:47:36 -0500

    Add camera flash

--- full_sequence.sh
+++ full_sequence.sh
@@ -4,5 +4,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 DATE=$(date +"%Y-%m-%d_%H%M")
+python $DIR/flash_on.py
 raspistill -vf -hf -o $DIR/pictures/$DATE.jpg
+python $DIR/flash_off.py
 python3 $DIR/write_power.py $DIR/pictures/$DATE.jpg >> $DIR/out.log
