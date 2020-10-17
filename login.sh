98d2d408a5f369263fc3e9ee85114ae7682f1ae7
jabbate7 <joseph.abbate7@gmail.com>
2020-10-16 22:11:13 -0400

    Allow settings update through website rather than through google sheets

--- login.sh
+++ login.sh
@@ -2,35 +2,63 @@
 
 LOGIN_URL="http://powerbucket.herokuapp.com/accounts/login/"
 SUBMISSION_URL="http://powerbucket.herokuapp.com/readings/submission/"
-TIME=$1
-FIRST_NUM=$2
-SECOND_NUM=$3
-THIRD_NUM=$4
-FOURTH_NUM=$5
-FIFTH_NUM=$6
+SETTINGS_URL="http://powerbucket.herokuapp.com/readings/change_settings/"
 YOUR_USER=$BUCKET_USER
 YOUR_PASS=$BUCKET_PASSWORD
 COOKIES=cookies.txt
 CURL_BIN="curl -s -c $COOKIES -b $COOKIES -e $LOGIN_URL"
 
-echo -n "Django Auth: get csrftoken ..."
+# echo -n "Django Auth: get csrftoken ..."
 $CURL_BIN $LOGIN_URL > /dev/null
 DJANGO_TOKEN="csrfmiddlewaretoken=$(grep csrftoken $COOKIES | sed 's/^.*csrftoken[[:space:]]*//')"
 
-echo -n " perform login ..."
+# echo -n " perform login ..."
 $CURL_BIN \
     -d "$DJANGO_TOKEN&username=$YOUR_USER&password=$YOUR_PASS" \
     -X POST $LOGIN_URL
 
-echo -n " do something while logged in ..."
-$CURL_BIN \
-    -d "$DJANGO_TOKEN" \
-    -X GET "$SUBMISSION_URL" > /dev/null
-DJANGO_TOKEN="csrfmiddlewaretoken=$(grep csrftoken $COOKIES | sed 's/^.*csrftoken[[:space:]]*//')"
+# echo -n " do something while logged in ..."
+if [[ "$1" == "check" ]]; then
+    # check whether user wants to update the settings
+    X=$($CURL_BIN \
+	    -d "$DJANGO_TOKEN" \
+	    -X GET "$SETTINGS_URL" | egrep -o 'name="x" value="[0-9]+"' | egrep -o '[0-9]+' )
+    Y=$($CURL_BIN \
+	    -d "$DJANGO_TOKEN" \
+	    -X GET "$SETTINGS_URL" | egrep -o 'name="y" value="[0-9]+"' | egrep -o '[0-9]+' )
+    R=$($CURL_BIN \
+	    -d "$DJANGO_TOKEN" \
+	    -X GET "$SETTINGS_URL" | egrep -o 'name="r" value="[0-9]+"' | egrep -o '[0-9]+' )
+    CHECKED=$($CURL_BIN \
+		  -d "$DJANGO_TOKEN" \
+		  -X GET "$SETTINGS_URL" | grep 'name="update"' | egrep -o 'checked' )
 
-$CURL_BIN \
-    -d "$DJANGO_TOKEN&time=$TIME&firstNum=$FIRST_NUM&secondNum=$SECOND_NUM&thirdNum=$THIRD_NUM&fourthNum=$FOURTH_NUM&fifthNum=$FIFTH_NUM" \
-    -X POST "$SUBMISSION_URL"
-
-echo " logout"
+    echo $CHECKED
+    echo $X
+    echo $Y
+    echo $R
+elif [[ "$1" == "update" ]]; then
+    $CURL_BIN \
+	-d "$DJANGO_TOKEN" \
+	-X GET "$SETTINGS_URL" > /dev/null
+    DJANGO_TOKEN="csrfmiddlewaretoken=$(grep csrftoken $COOKIES | sed 's/^.*csrftoken[[:space:]]*//')"
+    $CURL_BIN \
+	-d "$DJANGO_TOKEN&x=$2&y=$3&r=$4&update=False" \
+	-X POST "$SETTINGS_URL"
+elif [[ "$1" == "write" ]]; then 
+    TIME=$2
+    FIRST_NUM=$3
+    SECOND_NUM=$4
+    THIRD_NUM=$5
+    FOURTH_NUM=$6
+    FIFTH_NUM=$7
+    #write to the website
+    $CURL_BIN \
+	-d "$DJANGO_TOKEN" \
+	-X GET "$SUBMISSION_URL" > /dev/null
+    DJANGO_TOKEN="csrfmiddlewaretoken=$(grep csrftoken $COOKIES | sed 's/^.*csrftoken[[:space:]]*//')"
+    $CURL_BIN \
+	-d "$DJANGO_TOKEN&time=$TIME&firstNum=$FIRST_NUM&secondNum=$SECOND_NUM&thirdNum=$THIRD_NUM&fourthNum=$FOURTH_NUM&fifthNum=$FIFTH_NUM" \
+	-X POST "$SUBMISSION_URL"
+fi
 rm $COOKIES
--- write_power.py
+++ write_power.py
@@ -12,9 +12,10 @@ from scipy import signal
 import subprocess
 import os
 
-import pygsheets
-
 debug=False
+use_google=False
+if use_google:
+        import pygsheets
 
 base_dir=os.path.dirname(os.path.realpath(__file__))
 
@@ -49,28 +50,30 @@ def write_timestamp_and_power_scalar(wks,power,pic_time):
         wks.update_value('D{}'.format(rownum),pic_time)
         wks.update_value('E{}'.format(rownum),power)
 
-def write_timestamp_and_power_google(wks,power,pic_time):
-        col_names=['D','E','F','G','H','I']
-        rownum=wks.get_value('B9')
-        if rownum=='':
-                rownum=1
-                cell_val=wks.get_value('{}{}'.format(col_names[0],rownum))
-                while cell_val!='':
-                        rownum+=1
+if use_google:
+        def write_timestamp_and_power(wks,power,pic_time):
+                col_names=['D','E','F','G','H','I']
+                rownum=wks.get_value('B9')
+                if rownum=='':
+                        rownum=1
                         cell_val=wks.get_value('{}{}'.format(col_names[0],rownum))
-        wks.update_value('B9',int(rownum)+1)
-        wks.update_value('{}{}'.format(col_names[0],rownum),pic_time)
-        for i in range(len(power)):
-                wks.update_value('{}{}'.format(col_names[i+1],rownum),power[i])
+                        while cell_val!='':
+                                rownum+=1
+                                cell_val=wks.get_value('{}{}'.format(col_names[0],rownum))
+                wks.update_value('B9',int(rownum)+1)
+                wks.update_value('{}{}'.format(col_names[0],rownum),pic_time)
+                for i in range(len(power)):
+                        wks.update_value('{}{}'.format(col_names[i+1],rownum),power[i])
 
-def write_timestamp_and_power(power, pic_time):
-        command_list=[os.path.join(base_dir,'login.sh')]
-        command_list+=[str(pic_time)]
-        command_list+=[str(numeral) for numeral in power]
-        subprocess.check_call(command_list)
+else:
+        def write_timestamp_and_power(power, pic_time):
+                command_list=[os.path.join(base_dir,'login.sh'),'write']
+                command_list+=[str(pic_time)]
+                command_list+=[str(numeral) for numeral in power]
+                subprocess.check_call(command_list)
         
 
-def picture_to_power(picture_path,x,y,r,back_height_percentage,width_percentage,debug=False):
+def picture_to_power(picture_path,x,y,r,debug=False):
         
         image = Image.open(picture_path)
         image=image.convert('L') # monochrome
@@ -88,7 +91,7 @@ def picture_to_power(picture_path,x,y,r,back_height_percentage,width_percentage,
         for i,which_circle in enumerate(np.arange(num_circles)):
                 single_circle=cropped_image[:,which_circle*2*r:(which_circle+1)*2*r]
 
-                # length_correlation=int(len(thetas) * width_percentage)
+                # length_correlation=int(len(thetas) * .1) 
                 # filter_correlation=np.zeros(len(thetas))
                 # filter_correlation[:int(length_correlation/2)]=1
                 # filter_correlation[-int(length_correlation/2):]=1
@@ -275,20 +278,6 @@ def main():
                 else:
                         print(sys.argv[1])
                 prev_time=time.time()
-                
-        client=pygsheets.authorize()
-        if verbose:
-                next_time=time.time()
-                print('authorization time: {}'.format(next_time-prev_time))
-                prev_time=next_time
-
-        sh=client.open('173power')
-        if verbose:
-                next_time=time.time()
-                print('opening sheet time: {}'.format(next_time-prev_time))
-                prev_time=next_time
-        
-        wks=sh.sheet1
 
         # if the user doesn't specify a file do the test case...
         if len(sys.argv)<2:
@@ -296,14 +285,27 @@ def main():
                 print ('using default "joe_easy_test.jpg" ')
                 print('answer should be 89359.9')
                 picture_path='pictures/joe_easy_test.jpg'
-                x=1178
-                y=1340
-                r=133
-                back_height_percentage=.2 # .2 is for non-radial version
-                width_percentage=.11 #.3 is for non-radial version
-
+                # x=1178
+                # y=1340
+                # r=133
         else:
                 picture_path=sys.argv[1]
+
+        if use_google:
+                client=pygsheets.authorize()
+                if verbose:
+                        next_time=time.time()
+                        print('authorization time: {}'.format(next_time-prev_time))
+                        prev_time=next_time
+
+                sh=client.open('173power')
+                if verbose:
+                        next_time=time.time()
+                        print('opening sheet time: {}'.format(next_time-prev_time))
+                        prev_time=next_time
+
+                wks=sh.sheet1
+
                 if (wks.get_value('B7')!=''):
                         wks.update_value('B7','')
                         x,y,r=picture_to_circle_parameters(picture_path,
@@ -315,19 +317,33 @@ def main():
                         x=int(wks.get_value('B2'))
                         y=int(wks.get_value('B3'))
                         r=int(wks.get_value('B4'))
-                back_height_percentage=float(wks.get_value('B5'))
-                width_percentage=float(wks.get_value('B6'))
-                if verbose:
-                        next_time=time.time()
-                        print('getting parameters time: {}'.format(next_time-prev_time))
-                        prev_time=next_time
+        else:
+                command_list=[os.path.join(base_dir,'login.sh'),'check']
+                out=subprocess.check_output(command_list).decode('utf-8').split()
+                # a little jank: right now if the box is unchecked the update script returns one less output
+                # so we prepend unchecked just to keep everything the same length, if no output appears
+                # but if there is output that's not "checked" then it will still go smoothly 
+                if len(out)<4:
+                        out.insert(0,'unchecked')
+                if out[0]=='checked':
+                        x,y,r=picture_to_circle_parameters(picture_path,
+                                                           debug=debug)
+                        command_list=[os.path.join(base_dir,'login.sh'),'update',str(x),str(y),str(r)]
+                        subprocess.check_call(command_list)
+                else: 
+                        x=int(out[1])
+                        y=int(out[2])
+                        r=int(out[3])
+        if verbose:
+                next_time=time.time()
+                print('getting parameters time: {}'.format(next_time-prev_time))
+                prev_time=next_time
+                        
 
         power = picture_to_power(picture_path,
                                  x,
                                  y,
                                  r,
-                                 back_height_percentage,
-                                 width_percentage,
                                  debug=debug)
         if verbose:
                 next_time=time.time()
@@ -341,8 +357,10 @@ def main():
         # that long to calculate
         #pic_time=picture_path[:-4] 
 
-        write_timestamp_and_power(power,pic_time)
-        #write_timestamp_and_power(wks,power,pic_time)
+        if use_google:
+                write_timestamp_and_power(wks,power,pic_time)
+        else:
+                write_timestamp_and_power(power,pic_time)
         if verbose:
                 next_time=time.time()
                 print('writing power time: {}'.format(next_time-prev_time))
