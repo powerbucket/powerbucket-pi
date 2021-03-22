a89c206d52afac04bc082d9809b2abf523e26060
Joe Abbate <joseph.abbate7@gmail.com>
2021-03-21 23:00:13 -0400

    Update GPIO pin number (we changed the hardware since the last push

--- flash_off.py
+++ flash_off.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(16, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
 
-GPIO.output(16, False)
+GPIO.output(12, False)
 
 GPIO.cleanup()
--- flash_on.py
+++ flash_on.py
@@ -2,6 +2,6 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(16, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
 
-GPIO.output(16, True)
+GPIO.output(12, True)
